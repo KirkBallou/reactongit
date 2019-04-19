bd359391a0da74c4632c40e0f9975a9072fdccc0
kirkb <insomz@gmail.com>
2019-04-19 10:27:21 -0500

    Updates

---
+++ precache-manifest.8d88304891e5a1e56ed7d194c49e31bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactongit/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5628840442e4842aa14d",
+    "url": "/reactongit/static/js/runtime~main.f9415e0a.js"
+  },
+  {
+    "revision": "142797197f4bd3124abd",
+    "url": "/reactongit/static/js/main.9c637b72.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/reactongit/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "142797197f4bd3124abd",
+    "url": "/reactongit/static/css/main.c0031c18.chunk.css"
+  },
+  {
+    "revision": "8938dd04ff8002cbb6b62f4651217433",
+    "url": "/reactongit/index.html"
+  }
+];
